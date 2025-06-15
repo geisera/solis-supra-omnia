881bcc1bc6849ac6f9e7338d04bbf12ee5887f4e
Tony <geisera@gmail.com>
2025-06-15 18:27:40 -0400

    Changed h1 in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,6 +28,7 @@ const Header = () => {
           )}
         </div>
       </Link>
+      {siteMetadata.description}
       <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
         <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
           {headerNavLinks
