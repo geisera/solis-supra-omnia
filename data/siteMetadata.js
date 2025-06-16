08c4a1ccf5cfcf0ce1a58801f5d625f035324722
Tony <geisera@gmail.com>
2025-06-16 17:36:59 -0500

    Hero experiment....

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'SDC',
+  title: 'Sol Defense Corps',
   author: 'Tony Geiser',
-  headerTitle: 'SOL DEFENSE CORPS',
+  headerTitle: 'SDC',
   description: 'The journal of military science fiction',
   language: 'en-us',
   theme: 'system', // system, dark or light
