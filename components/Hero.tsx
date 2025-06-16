421866f0d6419288595ba00e81e62aa6c2b5e065
Tony <geisera@gmail.com>
2025-06-16 17:55:08 -0500

    Hero experiment

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,21 +8,13 @@ export default function Example() {
       <div className="relative isolate overflow-hidden">
         <div className="mx-auto max-w-7xl pb-24 pt-10 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-10">
           <div className="px-6 lg:px-0 lg:pt-4">
-            <h1 className="py-5 text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
+            <h1 className="hidden h-6 text-3xl font-semibold sm:block">
               Sol Defense Corps
             </h1>
             <h3 className="py-5 text-lg font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-lg sm:leading-10 md:text-2xl md:leading-14">
               The Journal of Military Science Fiction
             </h3>
-            {/* <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-              <ul>
-                <li> Books</li>
-                <li> Movies</li>
-                <li> Comics</li>
-                <li> and all things sci-fi!</li>
-              </ul>
-            </p> */}
-            <p>Welcome to Sol Defense Corps—your frontline outpost in the theater of military science fiction.</p>
+            <p>Welcome to Sol Defense Corps! Your frontline outpost in the theater of military science fiction.</p>
             <br />
             <p>Here, we review the latest novels, video games, movies, comics and holo-media.</p>
             <br />
