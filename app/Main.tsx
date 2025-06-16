e5c2b9cd0ce1585831e6bafc79dde91f7e42379e
Tony <geisera@gmail.com>
2025-06-16 07:25:43 -0400

    editing Main.tsx, h1, description color

--- app/Main.tsx
+++ app/Main.tsx
@@ -11,10 +11,10 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-yellow-200">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-yellow-600">
+          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-600">
             Latest Intel
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-yellow-400">
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
         </div>
