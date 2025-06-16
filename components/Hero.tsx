8ce2e792c26e7e2f549363d26e0e809ea0cc77eb
Tony <geisera@gmail.com>
2025-06-16 13:32:38 -0500

    Hero experiment 0.7

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,5 @@
+import Image from '@/components/Image'
+
 export default function Example() {
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
@@ -5,14 +7,14 @@ export default function Example() {
         <div className="mx-auto max-w-7xl pb-24 pt-10 sm:pb-32 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-10">
           <div className="px-6 lg:px-0 lg:pt-4">
             <h1 className="py-5 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-              he Journal of Military Science Fiction
+              The Journal of Military Science Fiction
             </h1>
             <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
               <ul>
                 <li> Books</li>
                 <li> Movies</li>
                 <li> Comics</li>
-                <li> and everything else!</li>
+                <li> and all things sci-fi!</li>
               </ul>
             </p>
           </div>
@@ -23,6 +25,8 @@ export default function Example() {
                   className="absolute -inset-y-px left-1/2 -z-10 ml-10 w-[200%] skew-x-[-30deg] bg-indigo-100 opacity-20 ring-1 ring-inset ring-white md:ml-20 lg:ml-36"
                   aria-hidden="true"
                 />
+                <Image src="/profile.png" />
+
                 {/* <div className="relative px-6 pt-8 sm:pt-16 md:pl-16 md:pr-0">
                   <div className="mx-auto max-w-2xl md:mx-0 md:max-w-none">
                     <div className="w-screen overflow-hidden rounded-tl-xl bg-gray-900">
