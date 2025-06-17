27c1d9f6837efc6437dd4b8c548aafc56e81833c
Tony <geisera@gmail.com>
2025-06-17 14:57:53 -0500

    New hero picture

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -46,7 +46,7 @@ export default function Example() {
         src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
         className="absolute inset-0 -z-10 size-full object-cover"
       /> */}
-      <Image src={source} alt={alt} />
+      <Image src={source} alt={alt} className="absolute inset-0 -z-10 size-full object-cover"/>
       <div
         aria-hidden="true"
         className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
